ed9c29acba59ce67affea55f3461d44567f012e2
Taukhid Aji Nurwijayadi <40711562+samCukid@users.noreply.github.com>
2023-07-08 08:38:18 +0700

    Update zcloudagentChecker.py

--- zcloudagentChecker.py
+++ zcloudagentChecker.py
@@ -16,6 +16,16 @@ print("2. Simple check")
 jawaban =  int(input("Silahkan pilih menu diatas: "))
 print("\n")
 
+print("\n===============================")
+print("=  Bulk ZCloud Agent Checker  =")
+print("===============================")
+print("1. Cek All Agent zCloud")
+print("2. Simple check")
+print("3. Monitoring All Agent zCloud")
+print("4. Keluar")
+jawaban =  int(input("Silahkan pilih menu diatas: "))
+print("\n")
+
 def serverwithAgent():    
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -49,7 +59,7 @@ def serverwithAgent():
                 val1 = td1.get_text()
                 val2 = td2.get_text()
                 print("Nama Server = ", val1)
-                print("Last Seen  = ", val2, "\n")
+                print("Last Seen   = ", val2, "\n")
 
 def  ngeceksuimpel():
     options = webdriver.ChromeOptions()
@@ -76,13 +86,36 @@ def  ngeceksuimpel():
         print("Zcloud aman boskuh :*\n")
     else:
         print("Zcloud rodok goyang, satset ",url)
-        
+
+def monitoring():
+    a = 0
+    while True:
+        if a != 25:
+            serverwithAgent()
+            a += 1
+            # print(f"monitor ke {a}")
+            time.sleep(5)
+        elif a == 25:
+            takon = str(input("Apakah anda ingin lanjut melakukan monitoring Y/N ?? "))
+            if takon == "Y" or takon == "y":
+                monitoring()
+            else:
+                sys.exit()
+    
+t = threading.Thread(target=monitoring)
+
 if jawaban == 1: 
     try:
         sc = requests.get(url)
         if sc.status_code == 200 :
             url1 = url
             serverwithAgent()
+        takon = str(input("Apakah anda ingin keluar dari aplikasi ini Y/N??"))
+        if takon == "Y":
+            print("Terimakasih salam kuli boskuh :*")
+            sys.exit()
+        else:
+            print("")
     except requests.exceptions.ConnectionError:
         print("Request error\n")
 elif jawaban == 2:
@@ -91,7 +124,24 @@ elif jawaban == 2:
         if sc.status_code == 200 :
             url1 = url
             ngeceksuimpel()
+        takon = str(input("Apakah anda ingin keluar dari aplikasi ini Y/N??"))
+        if takon == "Y":
+            print("Terimakasih salam kuli boskuh :*")
+            sys.exit()
+        else:
+            print("")
     except requests.exceptions.ConnectionError:
         print("Request Error\n")
+elif jawaban == 3:
+    try:
+        sc = requests.get(url)
+        if sc.status_code == 200 :
+            url1 = url
+            t.start()
+    except requests.exceptions.ConnectionError:
+        print("Request Error\n")
+elif jawaban == 4:
+    print("Terimakasih salam kuli boskuh :*")
+    sys.exit()
 else:
-    print("Silahkan masukan dengan benar ya bossss")
+    print("Silahkan masukan dengan benar boskuh XD")
